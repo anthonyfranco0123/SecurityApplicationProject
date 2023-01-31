72bf8eb2f03c2cc78f82bc24c9c252250f2b00ba
Immarail <ptrinh@hawk.iit.edu>
2023-01-30 20:24:00 -0600

    vertical bar worked

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:vertical_navigation_bar/vertical_navigation_bar.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+final pageController = PageController(
+    initialPage: 0,
+    keepPage: true
+);
+
 
 void main() {
   runApp(const MyApp());
@@ -49,68 +57,51 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+  final navItems = [
+    SideNavigationItem(icon: FontAwesomeIcons.calendarCheck, title: "New task"),
+    SideNavigationItem(icon: FontAwesomeIcons.calendarDays, title: "Personal task"),
+    SideNavigationItem(icon: FontAwesomeIcons.fileLines, title: "Personal document"),
+    SideNavigationItem(icon: FontAwesomeIcons.calendar, title: "Company task"),
+    SideNavigationItem(icon: FontAwesomeIcons.circleArrowRight, title: "Options")
+  ];
+  final initialTab = 0;
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: Row(
+        children: <Widget>[
+          SideNavigation(
+            navItems: this.navItems,
+            itemSelected: (index){
+              pageController.animateToPage(
+                  index,
+                  duration: Duration(milliseconds: 300),
+                  curve: Curves.linear
+              );
+            },
+            initialIndex: 0,
+            actions: <Widget>[
+              //add some action button here
+            ],
+          ),
+          Expanded(
+            child: PageView.builder(
+              itemCount: 5,
+              controller: pageController,
+              scrollDirection: Axis.vertical,
+              physics: NeverScrollableScrollPhysics(),
+              itemBuilder: (context, index){
+                return Container(
+                    color: Colors.blueGrey.withOpacity(0.1),
+                    child: Center(
+                      child: Text("Page " + index.toString()),
+                    )
+                );
+              },
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+          )
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
