2da28088e05ddaa4a17769f0bb6363833df5b7ee
Joanna <jfindura@hawk.iit.edu>
2023-02-06 19:31:33 -0600

    SystemPrivFile

---
+++ lib/SystemPrivileges.dart
@@ -0,0 +1,19 @@
+//import 'package:win32_registry/win32_registry.dart';
+//import 'dart:async';
+//import 'package:system_info2/system_info2.dart';
+//import 'package:process_run/shell.dart';
+//class SystemPrivileges {
+// void systemPrivilegesState(){
+  //final key1 = Registry.openPath(RegistryHive.localMachine,
+   //   path: r');
+  //final AlwaysInstallElevated = key1.getValueAsInt('');
+  //if (AlwaysInstallElevated != null) {
+   // if (AlwaysInstallElevated == 1) {
+    //  print('AlwaysInstallElevated state: on');
+    //}
+    //else {
+    //  print('AlwaysInstallElevated state: off');
+    //}
+  //}
+ //}/
+//}
