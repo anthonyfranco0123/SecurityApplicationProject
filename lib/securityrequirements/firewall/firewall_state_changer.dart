df2eb03bb7e88b41f5818d7165911c5777309835
anthonyfranco0123 <anthonyfranco0123@gmail.com>
2023-02-17 19:17:29 -0600

    Adding the missing file firewall_state_changer

---
+++ lib/securityrequirements/firewall/firewall_state_changer.dart
@@ -0,0 +1,12 @@
+import 'package:process_run/shell.dart';
+
+class FirewallStateChanger {
+  final _shell = Shell();
+  allFirewallStatesOn() {
+    _shell.run('''netsh advfirewall set allprofiles state on''');
+  }
+
+  allFirewallStatesOff() {
+    _shell.run('''netsh advfirewall set allprofiles state off''');
+  }
+}
