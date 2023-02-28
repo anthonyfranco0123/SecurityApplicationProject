9c34b162523611f508ec7f9abf46a76d91329141
Joanna <jfindura@hawk.iit.edu>
2023-02-27 18:47:13 -0600

    System Priv Requirement Registry Value Works

--- lib/SystemPrivileges.dart
+++ lib/SystemPrivileges.dart
@@ -1,20 +1,21 @@
 import 'package:win32_registry/win32_registry.dart';
   class SystemPrivileges {
-    static systemPrivilegesState(){
+    static systemPrivilegesState() {
       int state;
       final key1 = Registry.openPath(RegistryHive.localMachine,
-
-      path: r'SOFTWARE\Microsoft\PolicyManager\default\ApplicationManagement\MSIAlwaysInstallWithElevatedPrivileges');
-          final alwaysInstallElevated = key1.getValueAsInt('value');
+          path: r'SOFTWARE\Microsoft\PolicyManager\default\ApplicationManagement\MSIAlwaysInstallWithElevatedPrivileges');
+      final alwaysInstallElevated = key1.getValueAsString('value');
       print(alwaysInstallElevated);
-        if (alwaysInstallElevated == 1) {
-            print('AlwaysInstallElevated state: on');
-            state = 1;
-          }
-          else {
-            print('AlwaysInstallElevated state: off');
-            state = 0;
-          }
-
+      if (alwaysInstallElevated == "0x0") {
+        print('AlwaysInstallElevated state: off');
+        state = 1;
+      }
+      else if (alwaysInstallElevated == "0x1") {
+        print('AlwaysInstallElevated state: on');
+        state = 0;
+      }
+      else {
+        print('AlwaysInstallElevated registry value does not exist');
+      }
     }
   }
