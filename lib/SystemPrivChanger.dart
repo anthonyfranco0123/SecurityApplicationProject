f04a35c8234f77af8d782ba0c116a4a4988c1f10
Joanna <jfindura@hawk.iit.edu>
2023-02-27 18:26:35 -0600

    System Priv Requirement: Changer Added

---
+++ lib/SystemPrivChanger.dart
@@ -0,0 +1,14 @@
+import 'package:process_run/shell.dart';
+
+class SystemPrivChanger {
+  final _shell = Shell();
+
+  alwaysElevatedOff() {
+    _shell.run(
+        '''reg add HKLM\SOFTWARE\Microsoft\PolicyManager\default\ApplicationManagement\MSIAlwaysInstallWithElevatedPrivileges /v value /d 0x0 /f''');
+  }
+  alwaysElevatedOn() {
+    _shell.run(
+      '''reg add HKLM\SOFTWARE\Microsoft\PolicyManager\default\ApplicationManagement\MSIAlwaysInstallWithElevatedPrivileges /v value /d 0x1 /f''');
+  }
+}
