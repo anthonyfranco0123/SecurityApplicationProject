7df95b556c963e5fb87f4ce30a15f1107452a8d9
inikasingh <isingh9@hawk.iit.edu>
2023-03-06 19:57:08 -0600

    AUTOUPDATES

---
+++ lib/AutoUpdates.dart
@@ -0,0 +1,26 @@
+import 'package:win32_registry/win32_registry.dart';
+import 'dart:async';
+//import 'package:system_info2/system_info2.dart';
+import 'package:process_run/shell.dart';
+class AutoUpdates{
+        static getSystemUpdates() {
+        int AUStates = 0;
+        final key1 = Registry.openPath(RegistryHive.localMachine,
+            path: r'SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU');
+        final privateSUpdates = key1.getValueAsInt('NoAutoUpdate');
+        print(privateSUpdates);
+        if (privateSUpdates != null) {
+          if (privateSUpdates == 0) {
+            print('Auto Updates : on');
+            //AUStates = 0;
+          }
+          else {
+            print('Auto Updates : off');
+          }
+        }
+      }
+}
+
+
+uj6
+
