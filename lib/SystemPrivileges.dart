ef6aa9f61aed3e22302ca574a413469d86e562bc
Joanna <jfindura@hawk.iit.edu>
2023-03-20 19:47:50 -0500

    System Priv Requirement Fixed

--- lib/SystemPrivileges.dart
+++ lib/SystemPrivileges.dart
@@ -6,17 +6,17 @@ import 'package:win32_registry/win32_registry.dart';
           path: r'SOFTWARE\Microsoft\PolicyManager\default\ApplicationManagement\MSIAlwaysInstallWithElevatedPrivileges');
       final alwaysInstallElevated = key1.getValueAsString('value');
       //print(alwaysInstallElevated);
-      if (alwaysInstallElevated == "0x0") {
+      if (alwaysInstallElevated == "0x1") {
+        //print('AlwaysInstallElevated state: on');
+        state = 0;
+      }
+      else if (alwaysInstallElevated == "0x0") {
         //print('AlwaysInstallElevated state: off');
         state = 1;
       }
-      else if (alwaysInstallElevated == "0x1") {
-        //print('AlwaysInstallElevated state: on');
-        state = 2;
-      }
       else {
         //print('AlwaysInstallElevated registry value does not exist');
-        state = 3;
+        state = 2;
       }
       return state;
     }
