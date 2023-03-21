8b8df7f09b678e9613191bd4d45b9425f5f89f17
Immarail <ptrinh@hawk.iit.edu>
2023-03-20 19:27:44 -0500

    eventlog

--- lib/securityrequirements/event_logs.dart
+++ lib/securityrequirements/event_logs.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+import 'package:shell/shell.dart';
+
+import '../RegistryAccess.dart';
 class RequirementThreeWidget extends StatefulWidget {
 
   const RequirementThreeWidget({
@@ -11,6 +14,10 @@ class RequirementThreeWidget extends StatefulWidget {
 }
 
 class RequirementThreeWidgetState extends State<RequirementThreeWidget>{
+  var output;
+  var temp;
+  var display="";
+  bool isShown = false;
   @override
   void initState() {
     super.initState();
@@ -34,14 +41,83 @@ class RequirementThreeWidgetState extends State<RequirementThreeWidget>{
             ],
           ),
         ),
-        child: const Text('Req three',
-          style: TextStyle(
-            fontSize: 35,
-            color: Colors.white,
-          ),
-          textAlign: TextAlign.center,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const Text(
+              'Check Password Reset',
+              style: TextStyle(
+                fontSize: 35,
+                color: Colors.white,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            const Padding(padding: EdgeInsets.all(8.0)),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.black,
+                textStyle: const TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+              child: const Text('Get Password Reset Configuration'),
+              onPressed: () async {
+                display = '';
+                var shell = Shell();
+
+                //print(SysInfo.userName.toString());
+                output = await shell.startAndReadAsString('net', arguments: ['accounts']);
+                //var forceLogOff =  output.substring(output.indexOf("Force user logoff how long after time expires?:") + 54, output.indexOf("Minimum password age (days):") - 2 );
+                //var minpwage = output.substring(output.indexOf("Minimum password age (days):") + 54, output.indexOf("Maximum password age (days):")-2 );
+                var maxpwage = output.substring(output.indexOf("Maximum password age (days):") + 54, output.indexOf("Minimum password length:")-2 );
+                //var minpwlen = output.substring(output.indexOf("Minimum password length:") + 54, output.indexOf("Length of password history maintained:")-2 );
+                var pwhist = output.substring(output.indexOf("Length of password history maintained:") + 54, output.indexOf("Lockout threshold:")-2 );
+                //var lockoutThreshold = output.substring(output.indexOf("Lockout threshold:") + 54, output.indexOf("Lockout duration (minutes):")-2 );
+                //var lockoutDur = output.substring(output.indexOf("Lockout duration (minutes):") + 54, output.indexOf("Lockout observation window (minutes):")-2 );
+                //var lockoutObservation = output.substring(output.indexOf("Lockout observation window (minutes):") + 54, output.indexOf("Computer role:")-2 );
+                // print(output);
+                setState(() {
+                  isShown = true;
+                  //print(output);
+                  if (int.parse(maxpwage)!=90){
+                    display +="Password's max age is not ensured. Changed max age to 3 months.\n";
+                    RegistryAccess.changeMaxPwAge();
+                  }
+
+                  if(pwhist!="None"){
+                    if (int.parse(pwhist)!=10){
+                      display +="Password's history is not ensured. Changed number of stored password to 10.\n";
+                      RegistryAccess.changePwHist();
+                    }
+                  }
+                  else{
+                    display +="Password's history is not configured. Changed number of stored password to 10.\n";
+                    RegistryAccess.changePwHist();
+                  }
+                  //print(display.length);
+                });
+
+
+              },
+            ),
+            const Padding(padding: EdgeInsets.all(8.0)),
+            Visibility(
+              //if (bootStart == 0)
+              visible: isShown,
+              child: Text(
+                //'$output',
+                display.length!=0 ? "$display" : "All password policies are now ensured!",
+                style: TextStyle(
+                  color:display.length!=0 ? Colors.red : Colors.white,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+          ],
         ),
       ),
+
     );
   }
 }
